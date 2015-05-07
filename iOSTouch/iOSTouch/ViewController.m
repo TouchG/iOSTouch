7e350c97b09b6ad11a2ddca92167f8f6ac9f5614
touch <touchgsx@gmail.com>
2015-05-07 20:39:15 +0800

    Init project

---
+++ iOSTouch/iOSTouch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSTouch
+//
+//  Created by Touch on 15/5/7.
+//  Copyright (c) 2015年 ___TOUCH___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
